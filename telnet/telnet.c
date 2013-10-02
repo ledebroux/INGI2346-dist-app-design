52d463b729168897e27afc0d7af6abddd7b42820
ledebroux <leonard.debroux>
2013-10-02 14:28:04 +0200

    telnet

--- telnet/telnet.c
+++ telnet/telnet.c
@@ -4,34 +4,67 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#define TELNETD_PORT  7000
-#define M2_ADDR "130.104.172.88"
+#define TELNETD_PORT  8000
+//#define M2_ADDR "130.104.172.88"
+#define M2_ADDR "127.0.0.1"
 
 main(argc, argv) int    argc; char   *argv[ ];
 {
-    int sd1;
-    struct sockaddr_in     m2;
+  int sd1;
+  struct sockaddr_in     m2;
 
-    /* fill in the structure "m2" with the address of the
-     * server that we want to connect with */
-  
-    bzero((char *) &m2       , sizeof(m2));
-    m2.sin_family     = AF_INET;
-    m2.sin_addr.s_addr = inet_addr(M2_ADDR);
-    m2.sin_port       = htons(TELNETD_PORT);
+  /* fill in the structure "m2" with the address of the
+   * server that we want to connect with */
 
-    /* Open a TCP socket (an Internet stream socket)*/
+  bzero((char *) &m2       , sizeof(m2));
+  m2.sin_family     = AF_INET;
+  m2.sin_addr.s_addr = inet_addr(M2_ADDR);
+  m2.sin_port       = htons(TELNETD_PORT);
 
-    if ( (sd1 = socket(PF_INET, SOCK_STREAM, 0)) < 0){
-        perror("socket error in telnet");
-        exit(-1);
+  /* Open a TCP socket (an Internet stream socket)*/
+
+  if ( (sd1 = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+    perror("socket error in telnet");
+    exit(-1);
+  }
+
+  /* Connect to the server */
+
+  if (connect(sd1 , (struct sockaddr *) &m2 , sizeof ( m2 )) < 0){
+    perror("connect error in telnet");
+    exit(-1);
+  }
+  printf("ok");
+  char a[10];
+
+  while(strcmp(a, "close")) {
+    printf("Enter a command\n");
+    gets(a);
+    printf("comand %c", a[0]);
+    if(a[0]=='l'){
+      printf("Local command: %s\n", a);
     }
-
-    /* Connect to the server */
-
-    if (connect(sd1 , (struct sockaddr *) &m2 , sizeof ( m2 )) < 0){
-        perror("connect error in telnet");
-        exit(-1);
+    else{
+      printf("Distant host must perform: %s\n", a);
+      write(sd1, a, sizeof(a));
     }
-          printf("ok");
+    
+    printf("c: %lu", sizeof(a));
+    printf("c: %lu", sizeof("close"));
+    // int i;
+    // for(i=0; i<=sizeof(a); i++){
+    //   printf("%c\n", a[i]);
+    // }
+
+  }
+  // printf("Integer that you have entered is %s\n", a);
+
+  // write(sd1, a, sizeof(a));
+  // sleep(20);
+  // int i;
+  // for( i=0 ; ; i++ ){
+  //     if (i%1000000000==0){
+  //         printf("%i", i);
+  //     }
+  // } 
 }
--- telnet/telnetd.c
+++ telnet/telnetd.c
@@ -1,18 +1,21 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
-#define TELNETD_PORT 10023
+#define TELNETD_PORT 8000
 
 int sigflag;
 
 main (argc, argv) int argc; char *argv[ ];
 {
-   int sdw, sd2,clilen,childpid;
-   struct sockaddr_in m1,m2;
+  int sdw, sd2,clilen,childpid;
+  struct sockaddr_in m1,m2;
 
-      /* Prepare acknowledging child termination */
-      sigset(SIGCHLD, resquiescat);
+  /* Prepare acknowledging child termination */
+  //sigset(SIGCHLD, resquiescat);
 
 /*
 
@@ -21,88 +24,94 @@ main (argc, argv) int argc; char *argv[ ];
     En outre, l'événement SIGCHLD est masqué pendant l'exécution de "resquiescat". 
 
 */
+  // printf("1");
+  /* Open a TCP socket */
+  if ( ( sdw = socket (PF_INET, SOCK_STREAM, 0)) < 0){
+    perror("socket error in telnetd");
+    exit(-1);
+  }
 
-      /* Open a TCP socket */
-      if ( ( sdw = socket (PF_INET, SOCK_STREAM, 0)) < 0){
-          perror("socket error in telnetd");
-          exit(-1);
-      }
-
-      /* Bind our local address so that the client can send to us */
-      bzero((char *) &m2 , sizeof( m2 ));
+  /* Bind our local address so that the client can send to us */
+  bzero((char *) &m2 , sizeof( m2 ));
 
 /*
 
     La structure m2 a été allouée sur la pile. Il faut l'initialiser à zéro avec bzero parce que les prochaines instructions ne rempliront que quelques champs. Sans bzero, les autres champs auraient eu une valeur aléatoire. 
 
 */
+  // printf("2");
+  m2.sin_family      = AF_INET; /* address family : Internet */
+  m2.sin_addr.s_addr = htonl(INADDR_ANY); 
+  m2.sin_port        = htons(TELNETD_PORT);
 
-      m2.sin_family      = AF_INET; /* address family : Internet */
-      m2.sin_addr.s_addr = htonl(INADDR_ANY); 
-      m2.sin_port        = htons(TELNETD_PORT);
+  if (bind(sdw, (struct sockaddr *) &m2 , sizeof( m2 )) < 0){
+    perror("bind error in telnetd");
+    exit(-1);
+  }
 
-      if (bind(sdw, (struct sockaddr *) &m2 , sizeof( m2 )) < 0){
-          perror("bind error in telnetd");
-          exit(-1)
-      }
-
-      /* set up queue for connection request */
-      if(listen(sdw,5)<0){
-          perror("listen error in telnetd");
-          exit(-1);
-      }
+  /* set up queue for connection request */
+  if(listen(sdw,5)<0){
+    perror("listen error in telnetd");
+    exit(-1);
+  }
 
 /* DEUXIEME ETAPE (et troisième) */
 
-          clilen = sizeof(m1); 
-      for ( ; ; ) {
-           sigflag = 0;
-          /* Wait for a connection from a client process.
-           * This is an example of a concurrent server. */
+      clilen = sizeof(m1); 
+  for ( ; ; ) {
+    // printf("1");
+    sigflag = 0;
+      /* Wait for a connection from a client process.
+       * This is an example of a concurrent server. */
 
-         if ((sd2 = accept(sdw, (struct sockaddr *) &m1 , &clilen))<0)
-            /*Accept request from all network interfaces of any machine 
-             *(each network interface has a separate IP address) */
-         { 
+    if ((sd2 = accept(sdw, (struct sockaddr *) &m1 , &clilen))<0)
+        /*Accept request from all network interfaces of any machine 
+         *(each network interface has a separate IP address) */
+    { 
 
 /*
 
-    Si on arrive ici, c'est que accept ne s'est pas terminé normalement, c.-à-d. qu'il a renvoyé un code d'erreur (< 0) plutôt qu'un numéro de socket valide. Ceci peut se produire si accept a vraiment échoué ou s'il a été interrompu par un signal SIGCHLD. 
+Si on arrive ici, c'est que accept ne s'est pas terminé normalement, c.-à-d. qu'il a renvoyé un code d'erreur (< 0) plutôt qu'un numéro de socket valide. Ceci peut se produire si accept a vraiment échoué ou s'il a été interrompu par un signal SIGCHLD. 
 
 */
 
-            if(sigflag == 1)continue; 
-                   /* if we got SIGCHLD during accept, 
-                    * start a new iteration of the for loop */                                      
-             perror("accept error in telnetd");
-             exit(-1);
-          }
+      if(sigflag == 1)continue; 
+             /* if we got SIGCHLD during accept, 
+              * start a new iteration of the for loop */                                      
+      perror("accept error in telnetd");
+      exit(-1);
+    }
 
 /*
 
-    Souvenons nous que nous sommes dans une boucle for infinie
+Souvenons nous que nous sommes dans une boucle for infinie
 
-    A partir de la deuxième itération, un fils pourrait mourir à tout moment: ceci va produire un signal SIGCHLD.
-    Puisque le processus père passe la plupart de son temps dans l'appel système accept, SIGCHLD va probablement interrompre accept. Ceci n'est pas une erreur: il faut alors simplement recommencer accept.
+A partir de la deuxième itération, un fils pourrait mourir à tout moment: ceci va produire un signal SIGCHLD.
+Puisque le processus père passe la plupart de son temps dans l'appel système accept, SIGCHLD va probablement interrompre accept. Ceci n'est pas une erreur: il faut alors simplement recommencer accept.
 
 */
 
 /* QUATRIEME ETAPE */
 
-          if((childpid = fork()) < 0){
+    if((childpid = fork()) < 0){
+       perror("fork error in telnetd");
+       exit(-1);
+    }
 
-             perror("fork error in telnetd");
-             exit(-1);
-          }
+    else if (childpid == 0){
+      printf("pid 0");
+      close(sdw);
 
-          else if (childpid == 0){
-             close(sdw);
-                /*child process */
-             exit(0);
-          }
-        
-         close(sd2); /* parent process */
-      }
+      char a[10];
+      read(sd2, a, sizeof(a));
+      printf("I received %s\n", a);
+
+          /*child process */
+      exit(0);
+    }
+    
+    close(sd2); /* parent process */
+  }
 }
 
 /*
@@ -111,7 +120,7 @@ main (argc, argv) int argc; char *argv[ ];
 
 */
 
-int resquiescat(){wait();sigflag = 1;} /*called by SIGCHLD event handler*/
+//int resquiescat(){wait();sigflag = 1;} /*called by SIGCHLD event handler*/
 
 /*
 
