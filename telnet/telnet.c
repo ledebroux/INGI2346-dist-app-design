ac2d3515e735aedc8db059940a4f1c4edfb91da7
ledebroux <leonard.debroux>
2013-10-04 16:22:41 +0200

    functions

--- telnet/telnet.c
+++ telnet/telnet.c
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#define TELNETD_PORT  8008
+#define TELNETD_PORT  8010
 //#define M2_ADDR "130.104.172.88"
 #define M2_ADDR "127.0.0.1"
 
-int shrink(char*, char*);
+//int shrink(char*, char*);
+int getStringLength(char*, char);
+int fillString(char*, char*, int);
 
 main(argc, argv) int    argc; char   *argv[ ];
 {
@@ -44,24 +46,36 @@ main(argc, argv) int    argc; char   *argv[ ];
     fgets(buffer,255,stdin);
 
     /* Transfer from buffer to a char* input with the exact right size */
-    int i;
-    int j;
-    for(i=0; buffer[i] != '\0'; i++){}
-    i++;
+    // int i;
+    // for(i=0; buffer[i] != '\0'; i++){}
+    int i = getStringLength(buffer, 10);
     char str[i];
-    for(j=0; j<i; j++){
-      str[j] = buffer[j];
-    }
-    // printf("%lu", sizeof(str));
+    printf("leng i = %i\n", i);
+    printf("size str = %lu\n", sizeof(str));
+    fillString(buffer, str, i);
 
     char * tok;
     tok = strtok (str," ");
 
-    if(!strcmp(tok, "lpwd")){
+    int tok_length = getStringLength(tok, '\0');
+    char ftok[tok_length];
+    fillString(tok, ftok, tok_length);
+
+    // int j;
+    // for(j=0; j<i; j++){
+    //   printf("tok%i: %i\n", j, tok[j]);
+    //   if(tok[j] == '\n'){printf("youhou\n");}
+    // }
+
+    // printf("command = %s\n", ftok);
+    // printf("size = %lu\n", sizeof(str));
+    // printf("size tok = %lu\n", sizeof(ftok));
+
+    if(!strcmp(str, "lpwd")){
       printf("Local command: pwd\n");
     }
-    else if(!strcmp(tok, "lcp")){
-      printf("Local command: cp\n"); 
+    else if(!strcmp(tok, "lcd")){
+      printf("Local command: cd\n"); 
     }
     else if(!strcmp(tok, "lls")){
       printf("Local command: ls\n"); 
@@ -71,11 +85,21 @@ main(argc, argv) int    argc; char   *argv[ ];
     }
     else if(!strcmp(tok, "get")){
       printf("get\n");
-      printf("gettt %s\n", tok);
+      // printf("gettt %s\n", tok);
       write(sd1, tok, sizeof(str));
       tok = strtok (NULL, " ,.-");
-      printf("file %s\n", tok);
-      write(sd1, tok, sizeof(str));
+      tok_length = getStringLength(tok, '\0');
+      // int j;
+      // for(j=0; j<tok_length; j++){
+      //   printf("tok%i: %i\n", j, tok[j]);
+      //   if(tok[j] == '\0'){printf("youhou\n");}
+      // }
+      char temp[tok_length];
+      // printf("leng tok = %i\n", tok_length);
+      // printf("size tok = %lu\n", sizeof(temp));
+      fillString(tok, temp, tok_length);
+      printf("file %s\n", temp);
+      write(sd1, temp, sizeof(temp));
     }
     else if(!strcmp(tok, "put")){
       printf("put\n");
@@ -87,9 +111,37 @@ main(argc, argv) int    argc; char   *argv[ ];
 
     // while (tok != NULL)
     // {
+    //   printf("toksize: %lu", sizeof(tok));
     //   printf ("%s\n",tok);
     //   tok = strtok (NULL, " ,.-");
     // }
 
   } 
+}
+
+int getStringLength(char* str, char sep){
+  //printf("sep: %i", sep);
+  int i=0;
+  while(str[i] != sep){
+    // printf("char%i: %i\n", i, str[i]);
+    i++;
+  }
+  i++;
+  //i++;
+  // for(i=0; str[i] != sep; i++){}
+  return i;
+}
+
+int fillString(char* data, char* result, int length){
+  int j;
+  // printf("length : %i\n", length);
+  for(j=0; j<length; j++){
+    // printf("j : %i", j);
+    if(j != length-1){
+      result[j] = data[j];
+    } else {
+      result[j] = '\0';
+    }
+    
+  }
 }
