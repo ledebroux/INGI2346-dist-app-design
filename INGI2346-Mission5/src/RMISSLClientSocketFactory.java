0d6e1b8ccdaf449cd1f9f6d81e78dd26618f7a31
ledebroux <leonard.debroux@gmail.com>
2013-11-19 16:26:51 +0100

    modified version for authentication, no compiling so far

--- INGI2346-Mission5/src/RMISSLClientSocketFactory.java
+++ INGI2346-Mission5/src/RMISSLClientSocketFactory.java
@@ -1,23 +1,49 @@
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.Socket;
 import java.rmi.server.RMIClientSocketFactory;
+import java.security.KeyStore;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+import javax.security.cert.X509Certificate;
 
 public class RMISSLClientSocketFactory
-	implements RMIClientSocketFactory, Serializable {
+implements RMIClientSocketFactory, Serializable {
 
-    public Socket createSocket(String host, int port)
-	throws IOException
-	{
-	    SSLSocketFactory sf = null;
-	    SSLSocket s = null;
+	public Socket createSocket(String host, int port)
+			throws IOException
+			{
+		SSLSocketFactory sf = null;
+		SSLSocket s = null;
 
-	     sf = (SSLSocketFactory)SSLSocketFactory.getDefault();
+		try {
+			// set up key manager to do server authentication
+			SSLContext ctx;
+			KeyManagerFactory kmf;
+			KeyStore ks;
+			char[] passphrase = "clientpassphrase".toCharArray();
 
-	    s = (SSLSocket)sf.createSocket(host, port);
-	    return s;
-	}
+			ctx = SSLContext.getInstance("TLS");
+			kmf = KeyManagerFactory.getInstance("SunX509");
+			ks = KeyStore.getInstance("JKS");
+
+			ks.load(new FileInputStream("clienttestkeys"), passphrase);
+			kmf.init(ks, passphrase);
+			ctx.init(kmf.getKeyManagers(), null, null);
+
+			sf = ctx.getServerSocketFactory();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		//remplacé:
+		// sf = (SSLSocketFactory)SSLSocketFactory.getDefault();
+
+		s = (SSLSocket)sf.createSocket(host, port);
+		return s;
+			}
 }
--- INGI2346-Mission5/src/RMISSLServerSocketFactory.java
+++ INGI2346-Mission5/src/RMISSLServerSocketFactory.java
@@ -7,34 +7,35 @@ import javax.net.ssl.*;
 import javax.security.cert.X509Certificate;
 
 public class RMISSLServerSocketFactory
-	implements RMIServerSocketFactory, Serializable {
+implements RMIServerSocketFactory, Serializable {
 
-   public ServerSocket createServerSocket(int port)
-	  throws IOException
-  	{ 
-     SSLServerSocket ss = null;
-	    SSLServerSocketFactory ssf = null;
-	    try {
-	       	// set up key manager to do server authentication
-       		SSLContext ctx;
-       		KeyManagerFactory kmf;
-	       	KeyStore ks;
-	       	char[] filepassphrase = "serverfilepass".toCharArray();
-	       	char[] keypassphrase = "serverkeypass".toCharArray();
+	public ServerSocket createServerSocket(int port)
+			throws IOException
+			{ 
+		SSLServerSocket ss = null;
+		SSLServerSocketFactory ssf = null;
+		try {
+			// set up key manager to do server authentication
+			SSLContext ctx;
+			KeyManagerFactory kmf;
+			KeyStore ks;
+			char[] filepassphrase = "serverfilepass".toCharArray();
+			char[] keypassphrase = "serverkeypass".toCharArray();
 
-	       	ctx = SSLContext.getInstance("TLS");
-	       	kmf = KeyManagerFactory.getInstance("SunX509");
-	       	ks = KeyStore.getInstance("JKS");
+			ctx = SSLContext.getInstance("TLS");
+			kmf = KeyManagerFactory.getInstance("SunX509");
+			ks = KeyStore.getInstance("JKS");
 
-	       	ks.load(new FileInputStream("servertestkeys"), filepassphrase);
-	       	kmf.init(ks, keypassphrase);
-       		ctx.init(kmf.getKeyManagers(), null, null);
+			ks.load(new FileInputStream("servertestkeys"), filepassphrase);
+			kmf.init(ks, keypassphrase);
+			ctx.init(kmf.getKeyManagers(), null, null);
 
-      		ssf = ctx.getServerSocketFactory();
-	    } catch (Exception e) {
-       		e.printStackTrace();
-	    }
-     ss = (SSLServerSocket) ssf.createServerSocket(port);
-	    return ss;
- 	}
+			ssf = ctx.getServerSocketFactory();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		ss = (SSLServerSocket) ssf.createServerSocket(port);
+		ss.setNeedClientAuth(true);
+		return ss;
+			}
 }
