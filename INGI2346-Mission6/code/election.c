7cc34d510a8abfcc02930df08ddd9f5aad5e7419
ledebroux <leonard.debroux@gmail.com>
2013-12-17 17:12:14 +0100

    more work

--- INGI2346-Mission6/code/election.c
+++ INGI2346-Mission6/code/election.c
@@ -2,77 +2,180 @@
 #include <stdio.h>
 #include "pvm3.h"
 
+/*
+ * Receives a string sent by a task
+ */
 int receive(){
   char msg[100];
   int cc, tid;
-  cc = pvm_recv(-1,-1); /*wait for a message*/
-  pvm_bufinfo(cc,(int *)0,(int *)0,&tid); /*get tid of sender*/
-  pvm_upkstr(msg); /*extract string from receive buffer 
-                                 into greetings*/
+  cc = pvm_recv(-1,-1);
+  pvm_bufinfo(cc,(int *)0,(int *)0,&tid);
+  pvm_upkstr(msg);
   printf("from t%i: %s\n",tid,msg);
   return 0;
 }
 
-int main(){
+/*
+ * Computes the diameter of the graph
+ * Using the Floyd-Warshall algorithm to get
+ *    the shortest path between all nodes
+ */
+int compute_diameter(int vertices, unsigned int edges[][2]){
+  /* Begining of Floyd-Warshall algorithm */
+  int dist[vertices][vertices];
+  int i, j;
+  for (i=0; i<vertices; i++){
+    for(j=0; j<vertices; j++){
+      dist[i][j]=vertices;
+    }
+  }
+  for(i=0; i<vertices; i++){
+    dist[i][i]=0;
+  }
+  printf("size %lu\n", sizeof(edges)/sizeof(edges[0]));
+  for(j=0; j<(int)(sizeof(edges)/sizeof(edges[0])); j++){
+    int u = edges[j][0];
+    int v = edges[j][1];
+    dist[u][v] = 1;
+    printf("u: %i and v: %i\n", u, v);
+  }
+  int k;
+  for(k=0; k<vertices; k++){
+    for (i=0; i<vertices; i++){
+      for (j=0; j<vertices; j++){
+        if(dist[i][j] > dist[i][k] + dist[k][j]){
+          dist[i][j] = dist[i][k] + dist[k][j];
+        }
+      }
+    }
+  }
+  /* End of Floyd-Warshall algorithm */
+
+  for (i=0;i<vertices;i++){
+    for(j=0;j<vertices;j++){
+      printf("%d ",dist[i][j]);
+    }
+    printf("\n");
+  }
+ 
+  /* Computation of the diameter */
+  int diameter = 0;
+  for (i=0;i<vertices;i++){
+    for(j=0;j<vertices;j++){
+      if(dist[i][j]>diameter){
+        diameter = dist[i][j];
+      }
+    }
+  }
+  // printf("Diameter: %i\n", diameter);
+  return diameter;
+}
+
+/* 
+ * Prints an array of int
+ */
+void print_array(int* array, int size)
+{
+  int i;
+  for(i=0; i<size; i++){
+    printf("%i  ", array[i]);
+  }
+  printf("\n");
+}
+
+// void print_matrix(int** matrix, int row, int col)
+// {
+//   int i, j;
+//   for (i=0;i<row;i++){
+//     for(j=0;j<col;j++){
+//       printf("%d ",matrix[i][j]);
+//     }
+//     printf("\n");
+//   }
+// }
+
+int main()
+{
   unsigned int n = 5;
   unsigned int e[][2] = {{1,2}, {1,4}, {2,3}, {3,1}, {4,5}, {5,1}};
 
-  unsigned int i, j;
-  int adjMatrix[n-1][n-1];
+  /*
+   * VAR INIT
+   */
 
+  unsigned int i, j;
+
+  int diameter = compute_diameter(n, e);
+
+  int adjMatrix[n-1][n-1];
   for (i=0;i<n;i++){
     for(j=0;j<n;j++){
       adjMatrix[i][j]=0;
-      //printf("%d ",adjMatrix[i][j]);
     }
-    //printf("\n");
   }
 
   int cc;
-  int tid[n-1];
-  int reachable[n-1];
+  int tid[n-1]; /* Array containing the tid of each node */
 
+  int outgoing[n-1];
+  int ingoing[n-1];
   for(i = 1; i <= n; i++){
-    reachable[i-1]=0;
-    // loop to identify reachable nodes
+    outgoing[i-1]=0;
+    ingoing[i-1]=0;
+  }
+
+  /*
+   * Computation of AdjMatrix, outgoing and ingoing
+   * outgoing: array where outgoing[i] is 
+   *    the number of outgoing edges from i+1
+   * ingoing: array where ingoing[i] is 
+   *    the number of ingoing edges to i+1
+   */
+  for(i = 1; i <= n; i++){
     for(j=0; j<sizeof(e)/sizeof(e[0]); j++){
       if(e[j][0] == i){
-        //printf("neighbor: %i\n", e[j][1]);
         adjMatrix[i-1][e[j][1]-1] = 1;
-        reachable[i-1]++;
+        outgoing[i-1]++;
+        ingoing[e[j][1]-1]++;
       }
     }
+  }
+
+  /*
+   * Creates one pvm_task for each node in the graph
+   * and gives them as arg:
+   *    - outgoing
+   *    - ingoing
+   *    - id
+   */
+  for(i = 1; i <= n; i++){
     cc = pvm_spawn("node",(char **)0,0,"",1,&tid[i-1]);
     if(cc == 1){
-      //printf("%i Reachable from %i\n", reachable[i-1], i);
       pvm_initsend(PvmDataDefault);
-      pvm_pkint(&reachable[i-1], 1, 1);
+      pvm_pkint(&outgoing[i-1], 1, 1);
       pvm_send(tid[i-1],1);
-
-      //receive();
+      //receive(); /* if uncommented, a send() needs to be present at the right place in node.c */
     }
 
   }
-  //All tids are known.
 
-  // for (i=0;i<n;i++){
-  //   for(j=0;j<n;j++){
-  //     printf("%d ",adjMatrix[i][j]);
-  //   }
-  //   printf("\n");
-  // }
+  /* At this point of the execution, all tids are known */
 
-  for(i=0; i<n; i++){
-    printf("%i  ", tid[i]);
+  //print_matrix(adjMatrix, n, n);
+
+  for (i=0;i<n;i++){
+    for(j=0;j<n;j++){
+      printf("%d ",adjMatrix[i][j]);
+    }
+    printf("\n");
   }
-  printf("\n");
-  for(i=0; i<n; i++){
-    printf("%i  ", reachable[i]);
-  }
-  printf("\n");
+
+  print_array(tid, n);
+  print_array(outgoing, n);
+  print_array(ingoing, n);
 
   for(i=1; i<=n; i++){
-    int children[reachable[i-1]];
+    int children[outgoing[i-1]];
     int offset = 0;
     for(j=0; j<n; j++){
       if(adjMatrix[i-1][j]==1){
@@ -82,11 +185,12 @@ int main(){
       }
     }
     pvm_initsend(PvmDataDefault);
-    pvm_pkint(children, reachable[i-1], 1);
+    pvm_pkint(children, outgoing[i-1], 1);
     pvm_send(tid[i-1],1);
 
-    receive();
+    receive(); /* if uncommented, a send() needs to be present at the right place in node.c */
   }
+  printf("size of e %lu\n", sizeof(e)/sizeof(e[0]));
 
   printf("End of main()\n");
   return 0;
--- INGI2346-Mission6/code/node.c
+++ INGI2346-Mission6/code/node.c
@@ -4,44 +4,98 @@
 #include <string.h>
 #include "pvm3.h"
 
+/*
+ * Sends a string to a task
+ */
 int send(int* array, char* label, int tid, int size)
 {
-    //inform the creator that we know the reachables
   char msg[100];
   strcpy(msg,label);
   char str[2];
   int i;
+  /* Turn array into a string to be sent */
   for(i=0; i<size; i++){
     sprintf(str, " %d", array[i]);
-
     strcat(msg,str);
   }
 
-  pvm_initsend(PvmDataDefault); /*initialize send buffer*/
-  pvm_pkstr(msg); /*XDR encode string into send buffer*/
+  pvm_initsend(PvmDataDefault);
+  pvm_pkstr(msg);
   pvm_send(tid,1);
+  return 0;
 }
 
+/*
+ * Sends an id to a task
+ */
+int sendId(int tid, int max_id)
+{
+  pvm_initsend(PvmDataDefault);
+  pvm_pkint(&max_id, 1, 1);
+  pvm_send(tid,1);
+  return 0;
+}
+
+/*
+ * Receives an id sent by a task
+ */
+int receiveId()
+{
+  int cc;
+  int recvId[1];
+  cc = pvm_recv(-1,-1);
+  pvm_upkint(recvId, 1, 1);
+  return recvId[0];
+}
+
+/*
+ * Algorithm of election
+ */
+int election(int in, int out, int* children, int* max_id)
+{
+  int i;
+  for(i=0; i<out; i++){
+    sendId(children[i], *max_id);
+  }
+  for(i=0; i<in; i++){
+    int id = receiveId();
+    if(id > *max_id){
+      *max_id = id;
+    }
+  }
+  return 0;
+}
 
 int main()
 {
   int ptid;
+  int max_id; //TODO: needs to be set upon creation
+  int diameter; //TODO: needs to be set upon creation
+  int outgoing[1];
+  int ingoing[1];
 
   ptid=pvm_parent(); /*get tid of task that started me*/
 
+  /* the node receives the number of nodes it can reach */
   int cc;
-  int reachables[1];
-  cc = pvm_recv(-1,-1); /*wait for a message*/
-  pvm_upkint(reachables, 1, 1); /*extract string from receive buffer into greetings*/
-
-  //inform the creator that we know the reachables
-  //send(reachables, "My reachables: ", ptid, 1);
-
-  int children[reachables[0]];
   cc = pvm_recv(-1,-1);
-  pvm_upkint(children, reachables[0], 1);
+  pvm_upkint(outgoing, 1, 1);
 
-  send(children, "My children", ptid, reachables[0]);
+  /* inform the creator that we know the outgoing */
+  //send(outgoing, "My outgoing: ", ptid, 1); /* if uncommented, a receive() needs to be present at the right place in node.c */
+
+  /* the node receives the tid of the nodes it can reach */
+  int children[outgoing[0]];
+  cc = pvm_recv(-1,-1);
+  pvm_upkint(children, outgoing[0], 1);
+
+  /* inform the creator of the tid of the nodes it can reach */
+  send(children, "My children", ptid, outgoing[0]); /* if uncommented, a receive() needs to be present at the right place in node.c */
+
+  int i;
+  for(i=0; i<diameter; i++){
+    election(ingoing[0], outgoing[0], children, &max_id);
+  }
 
   exit(0);
 }
