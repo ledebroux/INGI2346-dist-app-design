57c829b9a408279cc88ca241bb4dc37068377437
ledebroux <leonard.debroux@gmail.com>
2013-12-17 14:52:31 +0100

    part 1

--- INGI2346-Mission6/code/election.c
+++ INGI2346-Mission6/code/election.c
@@ -1,80 +1,94 @@
 /* election.c */
 #include <stdio.h>
 #include "pvm3.h"
-int main()
-{
+
+int receive(){
+  char msg[100];
+  int cc, tid;
+  cc = pvm_recv(-1,-1); /*wait for a message*/
+  pvm_bufinfo(cc,(int *)0,(int *)0,&tid); /*get tid of sender*/
+  pvm_upkstr(msg); /*extract string from receive buffer 
+                                 into greetings*/
+  printf("from t%i: %s\n",tid,msg);
+  return 0;
+}
+
+int main(){
   unsigned int n = 5;
-  int e[][2] = {{1,2}, {1,4}, {2,3}, {3,3}, {4,5}, {5,1}};
+  unsigned int e[][2] = {{1,2}, {1,4}, {2,3}, {3,1}, {4,5}, {5,1}};
 
   unsigned int i, j;
   int adjMatrix[n-1][n-1];
 
-  for (i=0;i<n;i++)
-  {
-    for(j=0;j<n;j++)
-    {
+  for (i=0;i<n;i++){
+    for(j=0;j<n;j++){
       adjMatrix[i][j]=0;
       //printf("%d ",adjMatrix[i][j]);
     }
     //printf("\n");
   }
 
-  int reachable = 0;
-
   int cc;
   int tid[n-1];
+  int reachable[n-1];
 
-  for(i = 1; i <= n; i++)
-  {
+  for(i = 1; i <= n; i++){
+    reachable[i-1]=0;
     // loop to identify reachable nodes
     for(j=0; j<sizeof(e)/sizeof(e[0]); j++){
       if(e[j][0] == i){
-        adjMatrix[i][e[j][1]] = 1;
-        reachable++;
+        //printf("neighbor: %i\n", e[j][1]);
+        adjMatrix[i-1][e[j][1]-1] = 1;
+        reachable[i-1]++;
       }
     }
-    cc = pvm_spawn("node",(char **)0,0,"",1,&tid[i]);
+    cc = pvm_spawn("node",(char **)0,0,"",1,&tid[i-1]);
     if(cc == 1){
+      //printf("%i Reachable from %i\n", reachable[i-1], i);
       pvm_initsend(PvmDataDefault);
-      pvm_pkint(&reachable, 1, 1);
-      pvm_send(tid[i],1);
-      
-      // pvm_initsend(PvmDataDefault);
-      // pvm_pkint(&reachable, 1);
-      // pvm_send(tid[i],1);
+      pvm_pkint(&reachable[i-1], 1, 1);
+      pvm_send(tid[i-1],1);
+
+      //receive();
     }
-
-
-    // char * arg = (char*)&offset;
-
-    //   // cc = pvm_recv(-1,-1); /*wait for a message*/
-    //   // pvm_bufinfo(cc,(int *)0,(int *)0,&tid); /*get tid of sender*/
-    //   // pvm_upkstr(greetings); /*extract string from receive buffer 
-    //   //                                into greetings*/
-    //   // printf("from t%x: %s\n",tid,greetings);
-    // }else printf("Can't start task node\n");
-    //spawn a node with its reachable neighbors
 
   }
   //All tids are known.
 
+  // for (i=0;i<n;i++){
+  //   for(j=0;j<n;j++){
+  //     printf("%d ",adjMatrix[i][j]);
+  //   }
+  //   printf("\n");
+  // }
 
+  for(i=0; i<n; i++){
+    printf("%i  ", tid[i]);
+  }
+  printf("\n");
+  for(i=0; i<n; i++){
+    printf("%i  ", reachable[i]);
+  }
+  printf("\n");
 
-  // int cc, tid;
-  // char greetings[100];
+  for(i=1; i<=n; i++){
+    int children[reachable[i-1]];
+    int offset = 0;
+    for(j=0; j<n; j++){
+      if(adjMatrix[i-1][j]==1){
+        //printf("%i has child %i\n", i, tid[j]);
+        children[offset] = tid[j];
+        offset++;
+      }
+    }
+    pvm_initsend(PvmDataDefault);
+    pvm_pkint(children, reachable[i-1], 1);
+    pvm_send(tid[i-1],1);
 
-  // printf("I'm t %x\n",pvm_mytid());
-  // /* create the second task, executing program "hi" */
-  // cc = pvm_spawn("hi",(char **)0,0,"",1,&tid);
-  // if(cc == 1){
-  //         cc = pvm_recv(-1,-1); /*wait for a message*/
-  //         pvm_bufinfo(cc,(int *)0,(int *)0,&tid); /*get tid of sender*/
-  //         pvm_upkstr(greetings); /*extract string from receive buffer 
-  //                                        into greetings*/
-  //         printf("from t%x: %s\n",tid,greetings);
-  // }else printf("Can't start task hi\n");
-  // pvm_exit(); /*disconnect from the PVM*/
-  // exit(0);
-  printf("lalalal\n");
+    receive();
+  }
+
+  printf("End of main()\n");
   return 0;
 }
+
--- INGI2346-Mission6/code/node.c
+++ INGI2346-Mission6/code/node.c
@@ -1,37 +1,47 @@
 /* node.c */
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "pvm3.h"
+
+int send(int* array, char* label, int tid, int size)
+{
+    //inform the creator that we know the reachables
+  char msg[100];
+  strcpy(msg,label);
+  char str[2];
+  int i;
+  for(i=0; i<size; i++){
+    sprintf(str, " %d", array[i]);
+
+    strcat(msg,str);
+  }
+
+  pvm_initsend(PvmDataDefault); /*initialize send buffer*/
+  pvm_pkstr(msg); /*XDR encode string into send buffer*/
+  pvm_send(tid,1);
+}
+
+
 int main()
 {
   int ptid;
-  //char reachable = *argv[0];
-  //char greetings[100];
 
   ptid=pvm_parent(); /*get tid of task that started me*/
-  //strcpy(greetings,"Hello world from ");
-  // gethostname(greetings + strlen(greetings));
-
-  // pvm_initsend(PvmDataDefault); /*initialize send buffer*/
-  // pvm_pkstr(greetings); /*XDR encode string into send buffer*/
-  // pvm_send(ptid,1);
-
-  // printf("reachable %i\n", reachable);
-  // printf("ptid %i\n", ptid);
-  // pvm_exit(); /*disconnect from the PVM*/
 
   int cc;
-  int *reachables;
+  int reachables[1];
   cc = pvm_recv(-1,-1); /*wait for a message*/
-  // pvm_bufinfo(cc,(int *)0,(int *)0,&tid); /*get tid of sender*/
   pvm_upkint(reachables, 1, 1); /*extract string from receive buffer into greetings*/
-  
-  // int reach[reachables[0]];
-  // cc = pvm_recv(-1, -1);
-  // pvm_upkint(reach, 1);
 
-  // char status[20];
-  // cc = pvm_recv(-1,-1);
-  // pvm_upkstr(status);
+  //inform the creator that we know the reachables
+  //send(reachables, "My reachables: ", ptid, 1);
+
+  int children[reachables[0]];
+  cc = pvm_recv(-1,-1);
+  pvm_upkint(children, reachables[0], 1);
+
+  send(children, "My children", ptid, reachables[0]);
 
   exit(0);
 }
